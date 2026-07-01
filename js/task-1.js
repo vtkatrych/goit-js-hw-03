4565a684a7c6f9d8abf3c593446e7fc191847e42
Vitalii <katrychvitalii23@gmail.com>
2026-07-01 16:52:30 +0300

    finish

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+  const slug = title.toLowerCase().split(" ").join("-");
+  return slug;
+}
+
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const Array = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      Array.push(numbers[i]);
+    }
+  }
+  return Array;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
